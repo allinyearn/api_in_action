57fc9f2dbbb20a60b484853bd19cef4780bb009b
klo-kly <kalin.savelev07@gmail.com>
2021-09-09 02:45:09 +0300

    Примерно описал модели, сериализаторы и вьюшки для review и comments; настроил эндпоинты

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Comment, Review
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Comment, id=review_id)
+        queryset = review.comments.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs['review_id'])
+        serializer.save(author=self.request.user, review=review)
