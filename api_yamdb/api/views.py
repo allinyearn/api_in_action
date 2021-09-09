08c2422ea63e7c6159f4681375255bbc9fbce834
Александр <ruyearn@gmail.com>
2021-09-09 19:40:52 +0300

    добавил поиск по категориям и жанрам + фильтрацию по произведениям

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from reviews.models import Category, Genre, Title
 from . import serializers
@@ -7,13 +8,19 @@ from . import serializers
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', )
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = serializers.GenreSerializer
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('=name', )
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = serializers.TitleSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_fields = ('category', 'genre', 'name', 'year')
